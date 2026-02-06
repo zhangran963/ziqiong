c0ba2a9b06608bf7504105791942c5f406cf8eec
siyecao <ran>
2026-02-06 00:45:32 -0500

    chore: 重构11

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+# 必须先将 Rust 路径加入环境，否则云端找不到 cargo 命令
+export PATH="$HOME/.cargo/bin:$PATH"
+
 # 安装 wasm-pack
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
-# 执行 package.json 里的完整构建
+
+# 执行 package.json 中的 build 逻辑
 npm run build
